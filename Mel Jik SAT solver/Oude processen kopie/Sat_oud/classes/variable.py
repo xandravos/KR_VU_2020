673b2675efcf7af6b8913fe60c988954ec76cad5
Xandra Vos <vosxandra@gmail.com>
2020-09-14 21:41:56 +0200

    remove folder

--- Mel Jik SAT solver/Oude processen kopie/Sat_oud/classes/variable.py
+++
@@ -1,8 +0,0 @@
-class Variable:
-    def __init__(self, literal):
-        self.literal = literal
-        self.truth_value = None
-
-    def __str__(self):
-        s = str(self.literal) + " " + str(self.truth_value)
-        return s
